1578137b0d6968c6dbac869e8d5416b3e0c981b5
abcdefya <anh.dothe47@gmail.com>
2025-12-01 23:11:05 +0700

    .

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ terraform {
       version = "~> 2.0"
     }
   }
-  required_version = "1.5.6" // Terraform version
+  required_version = ">= 1.5.6" // Terraform version
 }
 
 data "google_client_config" "default" {}
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -15,10 +15,4 @@ provider "kubernetes" {
   token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(google_container_cluster.gke-cluster.master_auth[0].cluster_ca_certificate)
   
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "gke-gcloud-auth-plugin" # hoặc "gcloud"  gcloud auth application-default login
-    args        = ["get-credentials", google_container_cluster.gke-cluster.name, "--zone", google_container_cluster.gke-cluster.location, "--project", var.project_id]
-  }
-  config_path = "~/.kube/config"
 }
