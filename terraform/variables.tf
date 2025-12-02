dfe4c4e71c6922b01adb09d535af4dcdacead88f
abcdefya <anh.dothe47@gmail.com>
2025-12-02 17:46:21 +0000

    .

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,7 +2,7 @@
 // which can be accessed by var.project_id
 variable "project_id" {
   description = "The project ID to host the cluster in"
-  default     = "dongtd3"
+  default     = "binance-test-479915"
 }
 
 variable "zone" {
@@ -37,7 +37,7 @@ variable "machine_type" {
 
 variable "bucket" {
   description = "GCS bucket for MLE course"
-  default     = "dongtd2-bucket"
+  default     = "theanh-test-bucket-hihiaa"
 }
 
 variable "instance_name" {
@@ -77,7 +77,7 @@ variable "firewall_jenkins_source_ranges" {
 
 variable "ssh_keys" {
   description = "Value of the public ssh key"
-  default     = "dongtd6:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDAgRjELJmQLrMBFF1a50TP9psou4NF6c+gbagh8q+uBQ1NhkdC6gO5ymg3M34cZSQtKDSDZb+05h3NBXd6bZN19X1Eek1Kg5bZmozh3fu58TtLwWMW7rXIn95YqEnZe5Pn6b5v5FgsEW7IpzboYO92kRWIaRraFeJMPMFKP8mYH+ZiJZvefy9w/grgbAcBh0mXL/GciiI3vfRZ3j1IwLtK/cTf893Ik6E+ERDQYSpsqmGNnGaK10hQ8wTsz3omN7z0RdhoO6oauG6igxA5Gphb6V4MKbPGwfXuJCwumoqrVyXBBkvD2xBGUJQy7P/FnKb8+oKb5zlonLgujC8To+pMIFB/Mt0t6Ihp545AhuMnEI839SxJmk5a4hG8wZBSPtJ8DxlPb503o2aCtaOztcbOBl/M6rVLGUsaUt1IADFsDUIxqVGdPE5Y4cjmJ0/DID3tGJeWaV/zjEFhw4sS3CdlNMNKUjzkYZ7xIiLkF93EPzudNnO8/PjT5V2kYw3GSgU= dongtd6@AcerUbuntu"
+  default     = "testcoze:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQClvS99pi4MnrntIGMKxqQF7gk4bz8knUvGI22keNlPUZfT+BFcOtfsTCRAEflJMs9UDZNhCoyffBr83VKSSwkjeOXl0DZJfoTEgUBIaIy4fZHt2WfAf9OZrUMvbxQmUS1+4TQQZ5zPmSw3DxPGtBW3uhXySLCWfMsWisPRFEYmSX/+uiov0/tRS+alDt8eB5ghwUEzwpee6/RLdkxxyj64Js0pFvoABn18ZfaP1GzqxKabWnph6zry8+azlxjWMexxE4AxuQKtgTKtGPuPSuX5a9RfZtX4jTDEKZ9vM5cjS+TPZFqFzn86iSc6oohm4M0ncbJIW1J3S+zguuBROIWxAF7A9CbUKuuBcAUSmZZGI6lHE+3q74lrTnwIjM8VPONbr9P0mUCL0dpXMfM4lz3J5clZ19bussTm42EU0eH9RWiWXRAKOIzseZyWiKZgnu4ukRQ1dGyxxbhI+NE4947GDpwAwSJsQxWrSXOJc9gCefAN94x5wPN2Alr3d09c2D0= theanh"
 }
 // run command "ssh-keygen" to generate ssh keys
 // run command "cat ~/.ssh/id_rsa.pub" to show public ssh key
